cb48f087f6d946843ad32c3388b682756f3540e7
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-29 14:12:08 +0600

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+         for(char i='A';i<='Z';i++)
+       {
+           System.out.println(i);
+       }
+
     }
 }
